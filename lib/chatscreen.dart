6b2497538a0dfbb68675107984820fccc1195b17
kaelanwillauer <kaelanwillauer@gmail.com>
2021-11-04 16:55:04 -0700

    created sketch chatscreen

---
+++ lib/chatscreen.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/chatmessages.dart';
+
+//https://www.freecodecamp.org/news/build-a-chat-app-ui-with-flutter/
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    List<ChatMessage> messages = [
+      ChatMessage(
+          messageFrom: "Bill: ",
+          messageContent: "Hello, world!",
+          messageType: "receiver"),
+      ChatMessage(
+          messageFrom: "Susan: ",
+          messageContent: "Random text here!!!",
+          messageType: "receiver"),
+      ChatMessage(
+          messageFrom: "Bob: ",
+          messageContent: "Some question about game?",
+          messageType: "sender"),
+      ChatMessage(
+          messageFrom: "Auther: ",
+          messageContent: "CoooooooooLLLLLLLLLLL",
+          messageType: "receiver"),
+      ChatMessage(
+          messageFrom: "Bob: ",
+          messageContent: "LALALALALLALALALALA",
+          messageType: "sender"),
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Lobby Chat"),
+      ),
+      body: Stack(
+        children: <Widget>[
+          ListView.builder(
+            itemCount: messages.length,
+            shrinkWrap: true,
+            padding: const EdgeInsets.only(top: 10, bottom: 10),
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Container(
+                padding: const EdgeInsets.only(
+                    left: 14, right: 14, top: 10, bottom: 10),
+                child: Align(
+                  alignment: (messages[index].messageType == "receiver"
+                      ? Alignment.topLeft
+                      : Alignment.topRight),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: (messages[index].messageType == "receiver"
+                          ? Colors.grey.shade200
+                          : Colors.blue[200]),
+                    ),
+                    padding: const EdgeInsets.all(16),
+                    child: Text(
+                      messages[index].messageFrom +
+                          messages[index].messageContent,
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
+              height: 60,
+              width: double.infinity,
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Write message...",
+                          hintStyle: TextStyle(color: Colors.black54),
+                          border: InputBorder.none),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {}, // Create message
+                    child: const Icon(
+                      Icons.send,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                    backgroundColor: Colors.blue,
+                    elevation: 0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/chatscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Diplomacy',
       theme: ThemeData(
         // This is the theme of your application.
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Diplomacy Home Page'),
+      home: const ChatScreen(),
     );
   }
 }
