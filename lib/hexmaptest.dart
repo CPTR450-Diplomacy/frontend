c77496bc63c2257e7be1714b1f430adbf3d9f2d1
Ivan Guillen <ivanjguillen78@gmail.com>
2021-11-07 23:08:18 -0800

    Working with flutter_svg to produce provinces and CustomMultiChildLayout to position

--- lib/hexmaptest.dart
+++ lib/hexmaptest.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hexagon/hexagon.dart';
-import 'package:align_positioned/align_positioned.dart';
-import 'dart:ui' as ui;
-
-// code from https://pub.dev/packages/hexagon/versions/0.2.0-nullsafety.1/example
-// edited for debugging
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,70 +27,153 @@ class HexMapState extends State<HexMap> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('HexMap Example')),
-        body: Center(
-          child: CustomPaint(
-              size: Size(
-                  1000,
-                  (1600 * 0.5833333333333334)
-                      .toDouble()), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
-              painter: RPSCustomPainter()),
+        body: CustomMultiChildLayout(
+          delegate: MyDelegate(),
+          children: [
+            LayoutId(
+                id: 1,
+                child: SvgPicture.string(
+                  '''<svg version="1.1" width="800" height="800" xmlns="http://www.w3.org/2000/svg">
+  <g transform="scale(2,2)">
+    <g id="background-ead46bc7"/>
+    <g id="background-images-ead46bc7"/>
+    <g id="graphpaper-ead46bc7">
+      <g id="axis-ead46bc7">
+        <g>
+          <g>
+            <path fill="none" stroke="rgb(0,0,0)" class="dcg-svg-tickmark" paint-order="fill stroke markers" d="" stroke-opacity="0.9" stroke-miterlimit="2" stroke-width="1.5" stroke-dasharray=""/>
+          </g>
+        </g>
+      </g>
+    </g>
+    <g id="expressions-ead46bc7">
+      <g id="sketch-ead46bc7">
+        <title>Expression 20</title>
+        <path fill="#000000" stroke="none" class="dcg-svg-region" paint-order="stroke fill markers" d=" M 183.83682263700945 241.8689893873738 L 175.21834058812638 226.94134059458798 L 157.9813764903602 226.94134059458798 L 149.36289444147712 241.8689893873738 L 157.9813764903602 256.7966381801596 L 175.21834058812638 256.7966381801596 L 183.83682263700945 241.8689893873738" fill-opacity="0.4"/>
+      </g>
+      <g id="sketch-ead46bc7">
+        <title>Expression 21</title>
+        <path fill="#000000" stroke="none" class="dcg-svg-region" paint-order="stroke fill markers" d=" M 183.83682263700945 241.8689893873738 L 201.0737867347756 241.8689893873738 L 209.6922687836587 226.94134059458798 L 201.0737867347756 212.01369180180217 L 183.83682263700945 212.01369180180217 L 175.21834058812638 226.94134059458798 L 183.83682263700945 241.8689893873738" fill-opacity="0.4"/>
+      </g>
+      <g id="sketch-ead46bc7">
+        <title>Expression 22</title>
+        <path fill="#000000" stroke="none" class="dcg-svg-region" paint-order="stroke fill markers" d=" M 157.9813764903602 226.94134059458798 L 175.21834058812638 226.94134059458798 L 183.83682263700945 212.01369180180217 L 175.21834058812638 197.08604300901635 L 157.9813764903602 197.08604300901635 L 149.36289444147712 212.01369180180217 L 157.9813764903602 226.94134059458798" fill-opacity="0.4"/>
+      </g>
+      <g id="sketch-ead46bc7">
+        <title>Expression 23</title>
+        <path fill="#000000" stroke="none" class="dcg-svg-region" paint-order="stroke fill markers" d=" M 157.9813764903602 197.08604300901635 L 175.21834058812638 197.08604300901635 L 183.83682263700945 182.15839421623053 L 175.21834058812638 167.23074542344472 L 157.9813764903602 167.23074542344472 L 149.36289444147712 182.15839421623053 L 157.9813764903602 197.08604300901635" fill-opacity="0.4"/>
+      </g>
+      <g id="sketch-ead46bc7">
+        <title>Expression 25</title>
+        <path fill="#000000" stroke="none" class="dcg-svg-region" paint-order="stroke fill markers" d=" M 175.21834058812638 167.23074542344472 L 183.83682263700945 182.15839421623053 L 201.0737867347756 182.15839421623053 L 209.6922687836587 167.23074542344472 L 201.0737867347756 152.3030966306589 L 183.83682263700945 152.3030966306589 L 175.21834058812638 167.23074542344472" fill-opacity="0.4"/>
+      </g>
+      <g id="sketch-ead46bc7">
+        <title>Expression 26</title>
+        <path fill="#000000" stroke="none" class="dcg-svg-region" paint-order="stroke fill markers" d=" M 183.83682263700945 212.01369180180217 L 201.0737867347756 212.01369180180217 L 209.6922687836587 197.08604300901635 L 201.0737867347756 182.15839421623053 L 183.83682263700945 182.15839421623053 L 175.21834058812638 197.08604300901635 L 183.83682263700945 212.01369180180217" fill-opacity="0.4"/>
+      </g>
+      <g id="sketch-ead46bc7">
+        <title>Expression 27</title>
+        <path fill="#000000" stroke="none" class="dcg-svg-region" paint-order="stroke fill markers" d=" M 183.83682263700945 241.8689893873738 L 201.0737867347756 241.8689893873738 L 209.6922687836587 256.7966381801596 L 201.0737867347756 271.72428697294544 L 183.83682263700945 271.72428697294544 L 175.21834058812638 256.7966381801596 L 183.83682263700945 241.8689893873738" fill-opacity="0.4"/>
+      </g>
+    </g>
+    <g id="labels-ead46bc7"/>
+  </g>
+</svg>''',
+                  color: Colors.purpleAccent,
+                )),
+            LayoutId(
+                id: 2,
+                child: SvgPicture.string(
+                  '''<svg version="1.1" width="800" height="800" xmlns="http://www.w3.org/2000/svg">
+  <g transform="matrix(2, 0, 0, 2, 0, 0)">
+    <g id="background-dbcd4655"/>
+    <g id="background-images-dbcd4655"/>
+    <g id="graphpaper-dbcd4655">
+      <g id="axis-dbcd4655">
+        <g>
+          <g>
+            <path fill="none" stroke="rgb(0,0,0)" class="dcg-svg-tickmark" paint-order="fill stroke markers" d="" stroke-opacity="0.9" stroke-miterlimit="2" stroke-width="1.5" stroke-dasharray=""/>
+          </g>
+        </g>
+      </g>
+    </g>
+    <g id="expressions-dbcd4655">
+      <g id="sketch-dbcd4655">
+        <title>Expression 20</title>
+        <path fill="#000000" stroke="none" class="dcg-svg-region" paint-order="stroke fill markers" d=" M 192.21209764938044 233.82787424784638 L 184.1005851327846 219.7783224428715 L 167.8775600995929 219.7783224428715 L 159.76604758299706 233.82787424784638 L 167.8775600995929 247.87742605282125 L 184.1005851327846 247.87742605282125 L 192.21209764938044 233.82787424784638" fill-opacity="0.4"/>
+      </g>
+      <g id="sketch-dbcd4655">
+        <title>Expression 21</title>
+        <path fill="#000000" stroke="none" class="dcg-svg-region" paint-order="stroke fill markers" d=" M 192.21209764938044 233.82787424784638 L 208.43512268257214 233.82787424784638 L 216.54663519916798 219.7783224428715 L 208.43512268257214 205.7287706378966 L 192.21209764938044 205.7287706378966 L 184.1005851327846 219.7783224428715 L 192.21209764938044 233.82787424784638" fill-opacity="0.4"/>
+      </g>
+      <g id="sketch-dbcd4655">
+        <title>Expression 22</title>
+        <path fill="#000000" stroke="none" class="dcg-svg-region" paint-order="stroke fill markers" d=" M 167.8775600995929 219.7783224428715 L 184.1005851327846 219.7783224428715 L 192.21209764938044 205.7287706378966 L 184.1005851327846 191.67921883292175 L 167.8775600995929 191.67921883292175 L 159.76604758299706 205.7287706378966 L 167.8775600995929 219.7783224428715" fill-opacity="0.4"/>
+      </g>
+      <g id="sketch-dbcd4655">
+        <title>Expression 23</title>
+        <path fill="#000000" stroke="none" class="dcg-svg-region" paint-order="stroke fill markers" d=" M 167.8775600995929 191.67921883292175 L 184.1005851327846 191.67921883292175 L 192.21209764938044 177.62966702794685 L 184.1005851327846 163.58011522297193 L 167.8775600995929 163.58011522297193 L 159.76604758299706 177.62966702794685 L 167.8775600995929 191.67921883292175" fill-opacity="0.4"/>
+      </g>
+      <g id="sketch-dbcd4655">
+        <title>Expression 27</title>
+        <path fill="#000000" stroke="none" class="dcg-svg-region" paint-order="stroke fill markers" d=" M 192.21209764938044 233.82787424784638 L 208.43512268257214 233.82787424784638 L 216.54663519916798 247.87742605282125 L 208.43512268257214 261.92697785779615 L 192.21209764938044 261.92697785779615 L 184.1005851327846 247.87742605282125 L 192.21209764938044 233.82787424784638" fill-opacity="0.4"/>
+      </g>
+    </g>
+    <g id="labels-dbcd4655"/>
+  </g>
+</svg>''',
+                  color: Colors.blue,
+                ))
+          ],
         ));
   }
 }
 
-class RPSCustomPainter extends CustomPainter {
+class MyDelegate extends MultiChildLayoutDelegate {
+  MyDelegate({this.position});
+
+  final Offset? position;
+
   @override
-  void paint(Canvas canvas, Size size) {
-    Path path_1 = Path();
+  void performLayout(Size size) {
+    // `size` is the size of the `CustomMultiChildLayout` itself.
 
-    Paint paint_1_stroke = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = size.width * 0.001875000;
-    paint_1_stroke.color = Color.fromRGBO(0, 0, 0, 1.0).withOpacity(0.9);
-    canvas.drawPath(path_1, paint_1_stroke);
+    Size leadingSize = Size
+        .zero; // If there is no widget with id `1`, the size will remain at zero.
+    // Remember that `1` here can be any **id** - you specify them using LayoutId.
+    if (hasChild(1)) {
+      leadingSize = layoutChild(
+        1, // The id once again.
+        BoxConstraints.loose(
+            size), // This just says that the child cannot be bigger than the whole layout.
+      );
+      // No need to position this child if we want to have it at Offset(0, 0).
+    }
 
-    Path path_2 = Path();
-    path_2.moveTo(size.width * 0.2427360, size.height * 0.2633310);
-    path_2.lineTo(size.width * 0.2262679, size.height * 0.2348074);
-    path_2.lineTo(size.width * 0.1933317, size.height * 0.2348074);
-    path_2.lineTo(size.width * 0.1768636, size.height * 0.2633310);
-    path_2.lineTo(size.width * 0.1933317, size.height * 0.2918546);
-    path_2.lineTo(size.width * 0.2262679, size.height * 0.2918546);
-    path_2.lineTo(size.width * 0.2427360, size.height * 0.2633310);
+    if (hasChild(2)) {
+      final secondSize = layoutChild(
+        2,
+        BoxConstraints(
+          // This is exactly the same as above, but this can be anything you specify.
+          // BoxConstraints.loose is a shortcut to this.
+          maxWidth: size.width,
+          maxHeight: size.height,
+        ),
+      );
 
-    Paint paint_2_fill = Paint()..style = PaintingStyle.fill;
-    paint_2_fill.color = Color(0xff51B8F3).withOpacity(0.4);
-    canvas.drawPath(path_2, paint_2_fill);
-
-    Path path_5 = Path();
-    path_5.moveTo(size.width * 0.2427360, size.height * 0.2633310);
-    path_5.lineTo(size.width * 0.2756722, size.height * 0.2633310);
-    path_5.lineTo(size.width * 0.2921403, size.height * 0.2348074);
-    path_5.lineTo(size.width * 0.2756722, size.height * 0.2062838);
-    path_5.lineTo(size.width * 0.2427360, size.height * 0.2062838);
-    path_5.lineTo(size.width * 0.2262679, size.height * 0.2348074);
-    path_5.lineTo(size.width * 0.2427360, size.height * 0.2633310);
-
-    Paint paint_5_fill = Paint()..style = PaintingStyle.fill;
-    paint_5_fill.color = Color(0xffC8BC9C).withOpacity(0.4);
-    canvas.drawPath(path_5, paint_5_fill);
-
-    Path path_8 = Path();
-    path_8.moveTo(size.width * 0.1933317, size.height * 0.2348074);
-    path_8.lineTo(size.width * 0.2262679, size.height * 0.2348074);
-    path_8.lineTo(size.width * 0.2427360, size.height * 0.2062838);
-    path_8.lineTo(size.width * 0.2262679, size.height * 0.1777603);
-    path_8.lineTo(size.width * 0.1933317, size.height * 0.1777603);
-    path_8.lineTo(size.width * 0.1768636, size.height * 0.2062838);
-    path_8.lineTo(size.width * 0.1933317, size.height * 0.2348074);
-
-    Paint paint_8_fill = Paint()..style = PaintingStyle.fill;
-    paint_8_fill.color = Color(0xffC8BC9C).withOpacity(0.4);
-    canvas.drawPath(path_8, paint_8_fill);
+      positionChild(
+        2,
+        Offset(
+          leadingSize.width /
+              10, // This will place child 2 to the right of child 1.
+          14, // Centers the second child vertically.
+        ),
+      );
+    }
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
+  bool shouldRelayout(MyDelegate oldDelegate) {
+    return oldDelegate.position != position;
   }
 }
