f2c7071ac674c08a199918a6c53e5836a759a550
Your Name <owen.hoffman@wallawalla.edu>
2021-11-11 17:41:11 -0800

    default theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,16 +3,12 @@ import 'package:flutter/material.dart';
 class CustomTheme {
   static ThemeData get defaultTheme {
     return ThemeData(
+        brightness: Brightness.light,
         primaryColor: const Color(0xFFB9A37E),
-        scaffoldBackgroundColor: const Color(0xFFD1BE9D),
-        fontFamily: 'Behrensschrift',
-        buttonTheme: ButtonThemeData(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
-          buttonColor: const Color(0xFF3B727C),
-        ),
-        bannerTheme: const MaterialBannerThemeData(
-          backgroundColor: Color(0xFF3B727C),
+        backgroundColor: const Color(0xFFFAFAFA),
+        fontFamily: 'Akzidenz-grotesk',
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 60.0, fontFamily: 'Behrensschrift'),
         ));
   }
 }
