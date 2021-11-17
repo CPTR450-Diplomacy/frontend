6b02e4d7be0e14144f02f25dedc0efc9f8aa6456
Samuel Nguyen <samuel.nguyen@wallawalla.edu>
2021-11-16 17:42:29 -0800

    colors (hex)

--- lib/theme.dart
+++ lib/theme.dart
@@ -15,13 +15,13 @@ class CustomTheme {
 
 //Main 7 country colors (first - fill & second - outline)
 
-//Red: f0b097 & ed7648
-//Orange: f4cf99 & f4a93c
-//Yellow: f4dd99 & efc547
-//Green: bfd19d & 8ac489
-//Blue: b4ded9 & 7cc8d6
-//Indigo: 999ff4 & 6770f3
-//Violet: d399f4 & b64af3
+//Red: FFf0b097 & FFed7648
+//Orange: FFf4cf99 & FFf4a93c
+//Yellow: FFf4dd99 & FFefc547
+//Green: FFbfd19d & FF8ac489
+//Blue: FFb4ded9 & FF7cc8d6
+//Indigo: FF999ff4 & FF6770f3
+//Violet: FFd399f4 & FFb64af3
 
 //Map margins will be sand and the gray is an uninhabited country
 
