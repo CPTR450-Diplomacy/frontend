b6dfc435ac9fe53590f7e2a707da9cd3d3451b94
Your Name <owen.hoffman@wallawalla.edu>
2021-11-17 20:29:52 -0800

    testing

--- lib/widgets/course_details/course_details.dart
+++ lib/widgets/course_details/course_details.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:frontend/theme.dart';
 
 class CourseDetails extends StatelessWidget {
   const CourseDetails({Key? key}) : super(key: key);
@@ -16,15 +17,15 @@ class CourseDetails extends StatelessWidget {
         children: <Widget>[
           Text(
             'WELCOME TO\n DIPLOMACY',
-            style: TextStyle(
-                fontWeight: FontWeight.w800, height: 0.9, fontSize: 70),
+            style: CustomTheme.defaultTheme.textTheme.headline1,
           ),
           SizedBox(
             height: 30,
           ),
           Text(
-              'In this game, you are a country which controls three armies. Your goal is to move your armies to capture supply centers. Whichever country controls 18 supply centers wins the game. Happy Hunting.',
-              style: TextStyle(fontSize: 21, height: 1.7))
+            'In this game, you are a country which controls three armies. Your goal is to move your armies to capture supply centers. Whichever country controls 18 supply centers wins the game. Happy Hunting.',
+            style: CustomTheme.defaultTheme.textTheme.bodyText1,
+          )
         ],
       ),
     );
