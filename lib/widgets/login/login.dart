3c3c1acd655b391b07ea5ab669eb12d0b3196bcc
Owen <57083260+Owen-Hoffman@users.noreply.github.com>
2021-12-07 19:30:17 -0800

    Theme (#3)
    * first test
    
    * one more
    
    * main menu draft
    
    * testing
    
    * testing with widgets
    
    * colors (hex)
    
    * colors (hex)
    
    * colors (hex)
    
    * add Village font
    
    * add Village to theme
    
    * bodyText
    
    * test
    
    * testing
    
    * color test
    
    * testing
    
    * colors
    
    * testing
    
    * testing
    
    * testing
    
    * Centaur & Nadira
    
    * testing
    
    * established routing skeleton
    
    * finishing nav bar
    
    * Army and Fleet assets
    
    * test
    
    * cleaning code
    
    * theme
    
    * bug fixing
    
    * bug fix
    
    Co-authored-by: Elon Bontemps <jsmooveb13@gmail.com>
    Co-authored-by: Your Name <owen.hoffman@wallawalla.edu>
    Co-authored-by: Samuel Nguyen <samuel.nguyen@wallawalla.edu>

---
+++ lib/widgets/login/login.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/styling/theme.dart';
+
+//When merging branches make sure that the class name stays consistent with the one referenced in
+// main. The entire AppBar widget below should be implemented for navigation to work.
+
+class login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      centerTitle: true,
+      flexibleSpace: Image(
+        image: AssetImage('assets/pics/MicrosoftTeams-image.png'),
+        fit: BoxFit.cover,
+      ),
+      backgroundColor: Colors.transparent,
+      leading: Text(
+        'D',
+        style: CustomTheme.defaultTheme.appBarTheme.toolbarTextStyle,
+      ),
+      title: Text(
+        '',
+        style: CustomTheme.defaultTheme.appBarTheme.titleTextStyle,
+      ),
+      actions: <Widget>[
+        TextButton(
+          style: TextButton.styleFrom(
+            textStyle: CustomTheme.defaultTheme.textTheme.bodyText1,
+            primary: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, '/menuRoute');
+          },
+          child: const Text('Login'),
+        ),
+      ],
+    ));
+  }
+}
