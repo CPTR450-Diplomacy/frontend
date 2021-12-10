700f292d86b212b7fc05488d42a7a1bd54928db6
Elon Bontemps <73186850+elonbontemps@users.noreply.github.com>
2021-12-09 19:06:47 -0800

    Main menu (#5)
    * first test
    
    * one more
    
    * main menu draft
    
    * full page
    
    * up-to-date

---
+++ lib/views/home/home_view.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:frontend/widgets/call_to_action/call_to_action2.dart';
+import 'package:frontend/widgets/top_bar/top_bar2.dart';
+import 'package:frontend/widgets/centered_view/centered_view.dart';
+import 'package:frontend/widgets/course_details/course_details.dart';
+
+//this is the main website file for the menu
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: CenteredView(
+        child: Column(
+          children: <Widget>[
+            //TopBar2(),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  CourseDetails(),
+                  Expanded(
+                    child: Center(child: CallToAction2('Buttons')),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
